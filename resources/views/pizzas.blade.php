f3f1508b8b7ad11ee3f420cc82153d47703db307
Fred-S <fredvantage@gmail.com>
2021-02-04 19:10:38 +0100

    added DB

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height"></div>
-<div class="content">
-    <div class="title m-b-md">
-        Pizzas List
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizzas List
+        </div>
+        @foreach($pizzas as $pizza)
+            <div>
+                {{ $pizza->name }} - {{ $pizza->type }} - {{ $pizza->base }}
+            </div>
+        @endforeach
     </div>
-    @foreach($pizzas as $pizza)
-    <div>
-        {{ $pizza ['type'] }} - {{ $pizza ['base'] }}
-    </div>
-    @endforeach
-</div>
 </div>
 @endsection
